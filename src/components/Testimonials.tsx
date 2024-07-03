3ee5d57a7166fbf4f49020b5cd3492dacb93099e
Code_Junior <essienmichael4@gmail.com>
2024-07-03 16:01:47 +0000

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,8 +8,8 @@ const Testimonials = () => {
         <div className="flex flex-wrap justify-center">
             {testimonials.map((testimonial, i)=>{
                 return <div key={i} className='w-full sm:w-1/2 lg:w-1/3 px-4 py-2'>
-                    <div className="bg-neutral-50 h-full rounded-md p-6 text-md border border-neutral-200 font-thin">
-                        <p>{testimonial.text}</p>
+                    <div className="bg-neutral-50 h-full rounded-md p-6 text-md border border-neutral-300 ">
+                        <p className='text-neutral-500'>{testimonial.text}</p>
                         <div className='flex mt-8 items-start'>
                             <img src={testimonial.image} alt={testimonial.user} className='w-12 h-12 rounded-full mr-6 border border-neutral-500'/>
                             <div>
